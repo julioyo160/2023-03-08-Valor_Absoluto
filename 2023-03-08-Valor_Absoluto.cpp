6a1db452eb168f21aa7b62a3241df73fdc3f1b1f
julioyo160 <julioyo160@gmail.com>
2023-04-21 23:23:33 -0600

    Initial commit

---
+++ 2023-03-08-Valor_Absoluto.cpp
@@ -0,0 +1,34 @@
+// 2023-03-08-Valor_Absoluto.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//JULIO CESAR ESPINOZA VASQUEZ
+
+#include <iostream>
+
+
+
+int main()
+{
+    int numero;
+
+    std::cout << " Ingrese el número a evaluar: ";
+    std::cin >> numero;
+
+    if (numero >= 0) {
+        std::cout << "El valor absoluto es: " << numero << std::endl;
+    }
+    else {
+        numero *= -1;
+        std::cout << "El valor absoluto es: " << numero << std:: endl;
+    }
+
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
